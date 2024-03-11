58a5f0413a09f82511ffad770e92eb894fdda2ff
minwook-shin <minwook0106@gmail.com>
2024-03-11 22:36:52 +0900

    add python3 example for operator and dag

---
+++ example.py
@@ -0,0 +1,21 @@
+from f_scheduler import ConditionOperator, DefaultFunctionOperator, IterFunctionOperator, DAG, Converter
+
+
+def example_return_func(text):
+    return text
+
+
+dag = DAG()
+
+dag.add_task(DefaultFunctionOperator(function=print, param=(['hello']), task_id='hello_task'))
+dag.add_task(DefaultFunctionOperator(function=example_return_func, param=(['bye']), task_id='bye_task'))
+dag.add_task(ConditionOperator(10 > 1, task_id='condition_task'))
+dag.add_task(IterFunctionOperator(function=example_return_func, param=(['What your name?']), iterations=5, task_id='iter_task'))
+
+task_order = ['hello_task', 'condition_task', 'iter_task', 'bye_task']
+
+# or dag.set_downstream('function_task', 'condition_task')
+# dag.set_downstream('condition_task', 'iter_task')
+converter = Converter(dag)
+converter.convert_list_to_dag(task_order).run('hello_task')
+print(dag.tasks['iter_task'].return_value)
